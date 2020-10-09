03326ead4363b69f22143b9eb5b170d9b5dbe8f3
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-10-09 14:35:36 -0400

    edits

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ var d = new Date();
     year = d.getFullYear();
 
     document.getElementById("currentDay").innerHTML =d;
-   
+
+    $(".saveBtn").on("click", function(){
+        var input = document.getElementsByClassName("user-input");
+    alert("do you want to save these changes?");
+    localStorage.setItem("");
+    text = localStorage.getItem("user-input");
+    })
+
+    
+    
+    
+ 
