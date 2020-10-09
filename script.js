45c934f928e909cd1253658c07297419f39a6035
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-10-09 17:03:20 -0400

    scheduler object

--- script.js
+++ script.js
@@ -13,9 +13,25 @@ var thirteenInput = document.querySelector("#thirteen");
 var fourteenInput  = document.querySelector("#fourteen");
 var fifteenInput = document.querySelector("#fifteen");
 var sixteenInput = document.querySelector("#sixteen");
-var seventeenInput = document.querySelector("#seventeen");\
+var seventeenInput = document.querySelector("#seventeen");
 
+var scheduler = {
+    nineAM: nineInput.value.trim(),
+    tenAM: tenInput.value.trim(),
+    elevenAM: elevenInput.value.trim(),
+    twelvePM: twelveInput.value.trim(),
+    onePM: thirteenInput.value.trim(),
+    twoPM: fourteenInput.value.trim(),
+    threePM: fifteenInput.value.trim(),
+    fourPM: sixteenInput.value.trim(),
+    fivePM: seventeenInput.value.trim()
+  };
 
+  
+  //localStorage.setItem("user", JSON.stringify(user));
+
+  
+  //var lastUser = JSON.parse(localStorage.getItem("user"));
 
     //$(".saveBtn").on("click", function(){
         //var input = document.getElementsByClassName("user-input");
